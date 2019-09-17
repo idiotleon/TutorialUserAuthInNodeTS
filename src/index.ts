eacdef67d0fb9f70db7c1de3d1e36172adaa3e57
Leon <yanglyu.leon.7@gmail.com>
2019-09-16 20:05:20 -0700

    fea: Supporting User Authentication

--- src/index.ts
+++ src/index.ts
@@ -9,9 +9,10 @@ app.use(bodyParser.json());
 app.use('/api', routes);
 
 const PORT = 3777;
+const DBName = "idiotleon-user-auth-nodets";
 app.listen(PORT, async () => {
     console.log('Server ready!');
-    const mongoConnection = await mongoose.connect('mongodb://localhost/nodejs-auth');
+    const mongoConnection = await mongoose.connect(`mongodb://localhost/${DBName}`);
     console.log('Database ready!');
 
     console.log(`Listening on port ${PORT}`);
---
+++ src/services/items.ts
@@ -0,0 +1,9 @@
+import ItemModel from '../models/items';
+
+export default class ItemService {
+    constructor() { }
+
+    public async GetMyItem(user): Promise<any> {
+
+    }
+}
